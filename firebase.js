c2d9d39ddd832e2f66c3fe65d1d2b69b065b3c12
Cupcake-Legend <s160422020@student.ubaya.ac.id>
2024-12-08 20:32:36 +0700

    Add all

---
+++ firebase.js
@@ -0,0 +1,18 @@
+const { initializeApp } = require("firebase/app");
+const { getAuth } = require("firebase/auth");
+const { getFirestore } = require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+module.exports = { auth, db };
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const cheerio = require('cheerio');
-
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer"); // Tambahkan Puppeteer
+const { formatDistanceToNow } = require("date-fns");
+const { Builder, By, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const { auth, db } = require("./firebase");
+const {
+  doc,
+  setDoc,
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+} = require("firebase/firestore");
 const app = express();
 const port = 3000;
 
-app.use(cors());
-
-const stockCodes = ["^GSPC", "ADRO.JK", "ANTM.JK", "ASII.JK", "BBCA.JK", "BBNI.JK", "BBRI.JK", "BMRI.JK", "CTRA.JK", "GC=F", "GGRM.JK", "IDR=X", "INDF.JK", "INDY.JK", "LPKR.JK", "MYOR.JK", "PWON.JK", "UNVR.JK"]
-
+const stockCodes = ["^GSPC"];
 
 const processNewsPage = async (newsUrl, imgUrl) => {
   try {
@@ -22,15 +32,20 @@ const processNewsPage = async (newsUrl, imgUrl) => {
       return BigInt(milliseconds) * 1_000_000n; // Convert to nanoseconds
     };
 
-    const code = $('div.name').text().trim();
-    const title = $('h1.cover-title').text().trim();
-    const content = $('p').map((i, el) => $(el).text().trim()).get().join(' ');
-    const publisherAuthor = $('div.byline-attr-author').text().trim();
-    const percentageChange = $('fin-streamer.percentChange').text().trim();
-    const dateAttr = $('time.byline-attr-meta-time').attr('datetime');
+    const code = $("div.name").text().trim() || null;
+    const title = $("h1.cover-title").text().trim() || null;
+    const content =
+      $("p")
+        .map((i, el) => $(el).text().trim())
+        .get()
+        .join(" ") || null;
+    const publisherAuthor = $("div.byline-attr-author").text().trim() || null;
+    const percentageChange =
+      $("fin-streamer.percentChange").text().trim() || null;
+    const dateAttr = $("time.byline-attr-meta-time").attr("datetime") || null;
     const date = dateAttr ? new Date(dateAttr) : null;
-    const nanoSeconds = convertToNanoseconds(date);
-
+    const nanoSeconds = date ? convertToNanoseconds(date) : null;
+    console.log("success");
     return {
       code,
       title,
@@ -39,17 +54,33 @@ const processNewsPage = async (newsUrl, imgUrl) => {
       percentageChange,
       nanoSeconds,
       dateAttr,
-      dateText: $('time.byline-attr-meta-time').text(),
-      timeAgo: date ? formatDistanceToNow(date, { addSuffix: true }) : 'Unknown',
-      imgUrl
+      dateText: $("time.byline-attr-meta-time").text() || null,
+      timeAgo: date
+        ? formatDistanceToNow(date, { addSuffix: true })
+        : "Unknown",
+      imgUrl: imgUrl || null,
     };
   } catch (error) {
-    console.error('Error processing news page:', error.message);
-    return null;
+    // Log the error for debugging purposes
+    console.error("Error processing news page:", error.message);
+
+    // Return a default object with null or fallback values
+    return {
+      code: null,
+      title: null,
+      content: null,
+      publisherAuthor: null,
+      percentageChange: null,
+      nanoSeconds: null,
+      dateAttr: null,
+      dateText: null,
+      timeAgo: "Unknown",
+      imgUrl: imgUrl || null,
+    };
   }
 };
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     let allNewsContent = {};
 
@@ -60,73 +91,74 @@ app.get('/', async (req, res) => {
         const $ = cheerio.load(response.data);
         const uniqueNewsUrls = new Map();
 
-        $('div.stream-item.yf-186c5b2 a.subtle-link').each((index, element) => {
-          const href = $(element).attr('href');
-          const imgUrl = $(element).closest('div.stream-item').find('img.tw-bg-opacity-25').attr('src');
-          console.log("image: " + imgUrl);
+        $("div.stream-item.yf-186c5b2 a.subtle-link").each((index, element) => {
+          const href = $(element).attr("href");
+          const imgUrl = $(element)
+            .closest("div.stream-item")
+            .find("img.tw-bg-opacity-25")
+            .attr("src");
+          //console.log("image: " + imgUrl);
           if (href) {
-            const fullUrl = href.startsWith('http') ? href : `https://finance.yahoo.com${href}`;
+            const fullUrl = href.startsWith("http")
+              ? href
+              : `https://finance.yahoo.com${href}`;
             uniqueNewsUrls.set(fullUrl, imgUrl); // Simpan fullUrl sebagai key dan imgUrl sebagai value
           }
         });
-        
-        const limitedNewsUrls = Array.from(uniqueNewsUrls.entries()).slice(0, 1);
+
+        const limitedNewsUrls = Array.from(uniqueNewsUrls.entries()).slice(
+          0,
+          1
+        );
         console.log(limitedNewsUrls);
         const newsContents = await Promise.all(
           limitedNewsUrls.map(async ([fullUrl, imgUrl]) => {
-            console.log('Fetching URL:', fullUrl);
+            console.log("Fetching URL:", fullUrl);
             return await processNewsPage(fullUrl, imgUrl);
           })
         );
+
         allNewsContent[stockCode] = newsContents.filter(Boolean);
       })
     );
+    console.log("SUCCESSS");
     for (const key in allNewsContent) {
       if (Array.isArray(allNewsContent[key])) {
         // Filter out objects with empty "code"
-        allNewsContent[key] = allNewsContent[key].filter(item => item.code && item.code.trim() !== '');
+        allNewsContent[key] = allNewsContent[key].filter(
+          (item) => item.code && item.code.trim() !== ""
+        );
       }
     }
     // Convert BigInt to string for serialization
     const response = JSON.parse(
       JSON.stringify(allNewsContent, (key, value) =>
-        typeof value === 'bigint' ? value.toString() : value
+        typeof value === "bigint" ? value.toString() : value
       )
     );
+
+    
+
+
     res.json({
-      message: 'Crawling and news extraction completed!',
+      message: "Crawling and news extraction completed!",
       allNewsContent: response,
     });
   } catch (error) {
-    console.error('Error during crawling:', error.message);
-    res.status(500).send('An error occurred while crawling');
+    console.error("Error during crawling:", error.message);
+    res.status(500).send("An error occurred while crawling");
   }
 });
 
 // Endpoint untuk mengecek status URL
-app.get('/test', async (req, res) => {
-  const testUrl = req.query.url; // Ambil URL dari parameter query
-
-  if (!testUrl) {
-    return res.status(400).send('URL parameter is required');
-  }
-
+app.get("/test", async (req, res) => {
   try {
-    // Mengirimkan request untuk mengecek apakah URL dapat dijangkau
-    const response = await axios.get(testUrl);
-
-    // Jika request berhasil, kembalikan status code dan data
-    res.json({
-      message: 'URL is reachable!',
-      statusCode: response.status,
-      statusText: response.statusText,
-    });
+    const url = encodeURI("https://finance.yahoo.com/quote/BTC-USD/");
+    const response = await axios.get(url);
+    if (response) res.status(200).send("Success");
   } catch (error) {
-    // Jika gagal, kembalikan error
-    res.status(500).json({
-      message: 'Error accessing URL',
-      error: error.message,
-    });
+    console.error("ga nemu");
+    return null;
   }
 });
 
